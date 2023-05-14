4314b06002017304d7674c4338a421471ee502e7
Kayode George <68115973+kayodegeorge@users.noreply.github.com>
2023-05-14 23:37:50 +0100

    hildad about page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,28 @@
-import "./globals.css";
-import { neueMontreal } from "./styles/fonts";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import './globals.css'
+import { neueMontreal } from './styles/fonts'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
+import StartAProject from './components/StartAProject'
 
 export const metadata = {
-  title: "Abdulazeez Portfolio",
-  description: "Welcome to my world",
-};
+  title: 'Abdulazeez Portfolio',
+  description: 'Product Designer & No-code Developer',
+}
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={neueMontreal.className}>
         <Navbar />
 
         {children}
-
+        <StartAProject />
         <Footer />
       </body>
     </html>
-  );
+  )
 }
