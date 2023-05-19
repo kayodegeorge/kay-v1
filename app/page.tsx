a8ab605744f54acbc11f03417ecd332c08543ddb
Tobi Samuel <ufomatobi@gmail.com>
2023-05-19 14:39:54 +0100

    Add fill animation to buttons

--- app/page.tsx
+++ app/page.tsx
@@ -43,10 +43,12 @@ export default function Home() {
             engaging experiences that excites users and businesses.
           </p>
 
+          <button className=""></button>
+
           <div className="flex items-center">
             <Link
               href="/about"
-              className="flex w-fit items-center gap-3 rounded-full border border-black py-4 pl-6 pr-7"
+              className="relative flex w-fit items-center gap-3 overflow-hidden rounded-full border border-black py-4 pl-6 pr-7 before:absolute before:bottom-0 before:left-0 before:right-0 before:top-full before:-z-50 before:bg-black before:transition-[top] before:duration-200 before:ease-in before:content-[''] hover:fill-white hover:text-white hover:before:top-0"
             >
               <span className="uppercase">Learn more</span>
 
@@ -54,17 +56,11 @@ export default function Home() {
                 width="17"
                 height="15"
                 viewBox="0 0 17 15"
-                fill="none"
+                fill="inherit"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  d="M0 7.72571C0 7.34601 0.282154 7.03222 0.648229 6.98255L0.75 6.97571L15.75 6.97571C16.1642 6.97571 16.5 7.31149 16.5 7.72571C16.5 8.1054 16.2178 8.4192 15.8518 8.46886L15.75 8.47571L0.75 8.47571C0.335786 8.47571 0 8.13992 0 7.72571Z"
-                  fill="#0F141E"
-                />
-                <path
-                  d="M9.17101 2.23278C8.87749 1.94051 8.87646 1.46564 9.16872 1.17212C9.43442 0.905279 9.85103 0.880177 10.1451 1.0974L10.2294 1.16983L16.2794 7.19383C16.547 7.46032 16.5714 7.87843 16.3524 8.17251L16.2794 8.25673L10.2294 14.2817C9.93593 14.574 9.46105 14.573 9.16877 14.2795C8.90305 14.0127 8.87971 13.596 9.09817 13.3028L9.17096 13.2189L14.687 7.72501L9.17101 2.23278Z"
-                  fill="#0F141E"
-                />
+                <path d="M0 7.72571C0 7.34601 0.282154 7.03222 0.648229 6.98255L0.75 6.97571L15.75 6.97571C16.1642 6.97571 16.5 7.31149 16.5 7.72571C16.5 8.1054 16.2178 8.4192 15.8518 8.46886L15.75 8.47571L0.75 8.47571C0.335786 8.47571 0 8.13992 0 7.72571Z" />
+                <path d="M9.17101 2.23278C8.87749 1.94051 8.87646 1.46564 9.16872 1.17212C9.43442 0.905279 9.85103 0.880177 10.1451 1.0974L10.2294 1.16983L16.2794 7.19383C16.547 7.46032 16.5714 7.87843 16.3524 8.17251L16.2794 8.25673L10.2294 14.2817C9.93593 14.574 9.46105 14.573 9.16877 14.2795C8.90305 14.0127 8.87971 13.596 9.09817 13.3028L9.17096 13.2189L14.687 7.72501L9.17101 2.23278Z" />
               </svg>
             </Link>
           </div>
