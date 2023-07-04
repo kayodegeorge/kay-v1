a1b7bab15a2b8bace1609d3028e7b3ef519d4a6e
Tobi Samuel <ufomatobi@gmail.com>
2023-07-04 14:12:00 +0100

    Fix mobile menu issues

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,98 +1,101 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import Link from 'next/link'
+import Image from "next/image";
+import Link from "next/link";
 
 const projects = [
   {
-    title: 'Paypen',
+    title: "Paypen",
     description:
-      'A web app built solely for the purspose of employees pension payment',
-    image: 'assets/paypen-image.svg',
+      "A web app built solely for the purspose of employees pension payment",
+    image: "assets/paypen-image.svg",
     tags: [
-      'ui/ux design',
-      'web app',
-      'web design',
-      'interaction design',
-      'pension payment',
+      "ui/ux design",
+      "web app",
+      "web design",
+      "interaction design",
+      "pension payment",
     ],
-    url: '/work/paypen',
+    url: "/work/paypen",
   },
   {
-    title: 'Instaclean website',
+    title: "Instaclean website",
     description:
-      'Tech-powered marketplace for professional cleaning services in Nigeria',
-    image: 'assets/instaclean-image.svg',
-    tags: ['ui/ux design', 'web design', 'customer services'],
+      "Tech-powered marketplace for professional cleaning services in Nigeria",
+    image: "assets/instaclean-image.svg",
+    tags: ["ui/ux design", "web design", "customer services"],
+    url: "/work/paypen",
   },
   {
-    title: 'Gida App',
+    title: "Gida App",
     description:
-      'A mobile app that connects vetted professional cleaning service providers to customers',
-    image: 'assets/gida-image.svg',
+      "A mobile app that connects vetted professional cleaning service providers to customers",
+    image: "assets/gida-image.svg",
     tags: [
-      'ui/ux design',
-      'interaction design',
-      'product strategy',
-      'mobile app',
-      'customer service',
+      "ui/ux design",
+      "interaction design",
+      "product strategy",
+      "mobile app",
+      "customer service",
     ],
+    url: "/work/paypen",
   },
   {
-    title: 'Raha App',
+    title: "Raha App",
     description:
-      'A payment app that offers a fast, secure, and convenient way to make transactions',
-    image: 'assets/raha-image.svg',
+      "A payment app that offers a fast, secure, and convenient way to make transactions",
+    image: "assets/raha-image.svg",
     tags: [
-      'ui/ux design',
-      'mobile app',
-      'fintech',
-      'interaction design',
-      'product strategy',
+      "ui/ux design",
+      "mobile app",
+      "fintech",
+      "interaction design",
+      "product strategy",
     ],
+    url: "/work/paypen",
   },
-]
+];
 
 export default function Work() {
   return (
-    <div className='mt-6 px-4 lg:mt-[187px] lg:px-[100px]'>
-      <h1 className='text-4xl leading-[1.2] lg:text-[102px]'>
+    <div className="mt-6 px-4 lg:mt-[187px] lg:px-[100px]">
+      <h1 className="text-4xl leading-[1.2] lg:text-[102px]">
         Selected Projects
       </h1>
 
-      <div className='mt-24 space-y-14 border-b border-[#CFCFCF] pb-14 lg:mt-[120px]'>
+      <div className="mt-24 space-y-14 border-b border-[#CFCFCF] pb-14 lg:mt-[120px]">
         {projects.map((project) => (
           <div
             key={project.title}
-            className='border-t border-[#CFCFCF] pt-14 lg:grid lg:grid-cols-12 lg:gap-20'
+            className="border-t border-[#CFCFCF] pt-14 lg:grid lg:grid-cols-12 lg:gap-20"
           >
             <Link
-              href='/work'
-              className='relative block h-80 overflow-hidden lg:order-2 lg:col-span-7 lg:h-[489px]'
+              href={project.url}
+              className="relative block h-80 overflow-hidden lg:order-2 lg:col-span-7 lg:h-[489px]"
             >
               <Image
-                alt='Project Images'
+                alt="Project Images"
                 src={project.image}
                 fill
-                className='object-cover transition duration-500 ease-in-out hover:scale-110'
+                className="object-cover transition duration-500 ease-in-out hover:scale-110"
               />
             </Link>
 
-            <div className='mt-8 lg:order-1 lg:col-span-5 lg:mt-0 lg:flex lg:flex-col lg:justify-between'>
+            <div className="mt-8 lg:order-1 lg:col-span-5 lg:mt-0 lg:flex lg:flex-col lg:justify-between">
               <div>
-                <h2 className='text-2xl leading-[1.2] lg:text-[2rem]'>
+                <h2 className="text-2xl leading-[1.2] lg:text-[2rem]">
                   {project.title}
                 </h2>
 
-                <p className='mt-6 text-[20px] leading-6 text-[#828282]'>
+                <p className="mt-6 text-[20px] leading-6 text-[#828282]">
                   {project.description}
                 </p>
 
-                <div className='mt-6 flex flex-wrap gap-2'>
+                <div className="mt-6 flex flex-wrap gap-2">
                   {project.tags.map((tag, i) => (
                     <span
                       key={i}
-                      className='rounded-full border border-[#CCCCCC] px-4 py-2 text-sm uppercase text-[#314263]'
+                      className="rounded-full border border-[#CCCCCC] px-4 py-2 text-sm uppercase text-[#314263]"
                     >
                       {tag}
                     </span>
@@ -100,29 +103,21 @@ export default function Work() {
                 </div>
               </div>
 
-              <div className='mt-12 lg:mt-0'>
+              <div className="mt-12 lg:mt-0">
                 <Link
-                  href='/work'
-                  className='relative flex w-fit items-center gap-3 overflow-hidden rounded-full border border-black px-[24px] py-[14px] before:absolute before:bottom-0 before:left-0 before:right-0 before:top-full before:-z-50 before:bg-bg-lemon before:transition-[top] before:duration-200  before:ease-in before:content-[""] hover:border-bg-lemon hover:fill-black
-                  hover:before:top-0'
+                  href="/work"
+                  className='relative flex w-fit items-center gap-3 overflow-hidden rounded-full border border-black fill-black px-[24px] py-[14px] before:absolute before:bottom-0 before:left-0 before:right-0 before:top-full before:-z-50 before:bg-bg-lemon before:transition-[top] before:duration-200 before:ease-in before:content-[""] hover:border-bg-lemon hover:fill-white hover:text-white hover:before:top-0'
                 >
-                  <span className='uppercase text-white'>Read case study</span>
+                  <span className="uppercase">Read case study</span>
 
                   <svg
-                    width='17'
-                    height='15'
-                    viewBox='0 0 17 15'
-                    fill='none'
-                    xmlns='http://www.w3.org/2000/svg'
+                    width="17"
+                    height="15"
+                    viewBox="0 0 17 15"
+                    xmlns="http://www.w3.org/2000/svg"
                   >
-                    <path
-                      d='M0 7.72571C0 7.34601 0.282154 7.03222 0.648229 6.98255L0.75 6.97571L15.75 6.97571C16.1642 6.97571 16.5 7.31149 16.5 7.72571C16.5 8.1054 16.2178 8.4192 15.8518 8.46886L15.75 8.47571L0.75 8.47571C0.335786 8.47571 0 8.13992 0 7.72571Z'
-                      fill='#FFFFFF'
-                    />
-                    <path
-                      d='M9.17101 2.23278C8.87749 1.94051 8.87646 1.46564 9.16872 1.17212C9.43442 0.905279 9.85103 0.880177 10.1451 1.0974L10.2294 1.16983L16.2794 7.19383C16.547 7.46032 16.5714 7.87843 16.3524 8.17251L16.2794 8.25673L10.2294 14.2817C9.93593 14.574 9.46105 14.573 9.16877 14.2795C8.90305 14.0127 8.87971 13.596 9.09817 13.3028L9.17096 13.2189L14.687 7.72501L9.17101 2.23278Z'
-                      fill='#FFFFFF'
-                    />
+                    <path d="M0 7.72571C0 7.34601 0.282154 7.03222 0.648229 6.98255L0.75 6.97571L15.75 6.97571C16.1642 6.97571 16.5 7.31149 16.5 7.72571C16.5 8.1054 16.2178 8.4192 15.8518 8.46886L15.75 8.47571L0.75 8.47571C0.335786 8.47571 0 8.13992 0 7.72571Z" />
+                    <path d="M9.17101 2.23278C8.87749 1.94051 8.87646 1.46564 9.16872 1.17212C9.43442 0.905279 9.85103 0.880177 10.1451 1.0974L10.2294 1.16983L16.2794 7.19383C16.547 7.46032 16.5714 7.87843 16.3524 8.17251L16.2794 8.25673L10.2294 14.2817C9.93593 14.574 9.46105 14.573 9.16877 14.2795C8.90305 14.0127 8.87971 13.596 9.09817 13.3028L9.17096 13.2189L14.687 7.72501L9.17101 2.23278Z" />
                   </svg>
                 </Link>
               </div>
@@ -131,5 +126,5 @@ export default function Work() {
         ))}
       </div>
     </div>
-  )
+  );
 }
--- app/work/paypen/page.tsx
+++ app/work/paypen/page.tsx
@@ -1,35 +1,35 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 const Paypen = () => {
   return (
-    <div className=''>
-      <div className='relative h-[294px] w-full lg:h-[calc(100vh-88px)]'>
+    <div className="">
+      <div className="relative h-[294px] w-full lg:h-[calc(100vh-88px)]">
         <Image
-          alt='Paypen Case Study Image'
-          src='/assets/paypen-case-study-1.svg'
+          alt="Paypen Case Study Image"
+          src="/assets/paypen-case-study-1.svg"
           fill
-          className='object-cover object-bottom'
+          className="object-cover object-bottom"
         />
       </div>
 
-      <section className=' mt-[118px] px-4'>
-        <div className='mx-auto max-w-4xl'>
-          <div className='flex flex-col justify-between gap-6 lg:flex-row'>
-            <div className='flex flex-col'>
-              <span className=''>Year</span>
-              <span className=''>2022</span>
+      <section className=" mt-[118px] px-4">
+        <div className="mx-auto max-w-4xl">
+          <div className="flex flex-col justify-between gap-6 lg:flex-row">
+            <div className="flex flex-col">
+              <span className="">Year</span>
+              <span className="">2022</span>
             </div>
-            <div className='flex flex-col'>
-              <span className=''>Role</span>
-              <span className=''>UI design, Interaction design</span>
+            <div className="flex flex-col">
+              <span className="">Role</span>
+              <span className="">UI design, Interaction design</span>
             </div>
-            <div className='flex flex-col'>
-              <span className=''>Website</span>
-              <span className=''>Live Website</span>
+            <div className="flex flex-col">
+              <span className="">Website</span>
+              <span className="">Live Website</span>
             </div>
           </div>
-          <h1 className='mt-24 text-4xl font-medium'>Overview</h1>
-          <p className='text-2xl leading-9'>
+          <h1 className="mt-24 text-4xl font-medium">Overview</h1>
+          <p className="text-2xl leading-9">
             PayPen is an application built to tackle the remittance and
             reconciliation issues currently faced in the Nigerian pension
             industry today. It ensures that all pension remittances made come
@@ -38,9 +38,9 @@ const Paypen = () => {
             of the concerned Nigerian employees.
           </p>
         </div>
-        <div className='mx-auto max-w-4xl'>
-          <h1 className='text-4xl font-medium'>Problem</h1>
-          <p className='text-2xl leading-9'>
+        <div className="mx-auto max-w-4xl">
+          <h1 className="text-4xl font-medium">Problem</h1>
+          <p className="text-2xl leading-9">
             In Nigeria today, all Organizations with three or more employees
             must make pension remittances on behalf of their staff to give them
             a means of livelihood when they retire. However, there is a problem
@@ -55,56 +55,56 @@ const Paypen = () => {
             are no returns for all parties involved.
           </p>
         </div>
-        <div className='mx-auto max-w-4xl'>
-          <h1 className='text-4xl font-medium'>Goal</h1>
-          <p className='text-2xl leading-9'>
+        <div className="mx-auto max-w-4xl">
+          <h1 className="text-4xl font-medium">Goal</h1>
+          <p className="text-2xl leading-9">
             PayPen provides an electronic platform where Employers can make
             payments seamlessly while ensuring that a schedule is provided and
             transmitted to the pension operators for onward processing.
           </p>
         </div>
-        <div className='mx-auto max-w-4xl'>
-          <h1 className='text-4xl font-medium'>Role</h1>
-          <p className='text-2xl leading-9'>
+        <div className="mx-auto max-w-4xl">
+          <h1 className="text-4xl font-medium">Role</h1>
+          <p className="text-2xl leading-9">
             Visual Design and Interaction: Designed the Wireframes, HiFidelity
             prototypes and System style guide.
           </p>
         </div>
-        <div className='mx-auto max-w-4xl'>
-          <h1 className='text-4xl font-medium'>Sketches</h1>
-          <p className='text-2xl leading-9'>
+        <div className="mx-auto max-w-4xl">
+          <h1 className="text-4xl font-medium">Sketches</h1>
+          <p className="text-2xl leading-9">
             I used the user research and information architecture created my the
             product team to brainstorm and sketch down by ideas
           </p>
         </div>
-        <div className='first-letter: mt-[80px] grid grid-cols-3 gap-1.5 lg:gap-5 '>
-          <div className='relative w-full'>
+        <div className="first-letter: mt-[80px] grid grid-cols-3 gap-1.5 lg:gap-5 ">
+          <div className="relative w-full">
             <Image
-              alt='Sketch images'
-              src='/assets/sketch-1.svg'
-              className='object'
+              alt="Sketch images"
+              src="/assets/sketch-1.svg"
+              className="object"
               fill
             />
           </div>
-          <div className='relative w-full'>
-            <Image alt='Sketch images' src='/assets/sketch-1.svg' fill />
+          <div className="relative w-full">
+            <Image alt="Sketch images" src="/assets/sketch-1.svg" fill />
           </div>
-          <div className='relative w-full'>
-            <Image alt='Sketch images' src='/assets/sketch-1.svg' fill />
+          <div className="relative w-full">
+            <Image alt="Sketch images" src="/assets/sketch-1.svg" fill />
           </div>
-          <div className='relative w-full'>
-            <Image alt='Sketch images' src='/assets/sketch-1.svg' fill />
+          <div className="relative w-full">
+            <Image alt="Sketch images" src="/assets/sketch-1.svg" fill />
           </div>
-          <div className='relative w-full'>
-            <Image alt='Sketch images' src='/assets/sketch-1.svg' fill />
+          <div className="relative w-full">
+            <Image alt="Sketch images" src="/assets/sketch-1.svg" fill />
           </div>
-          <div className='relative w-full'>
-            <Image alt='Sketch images' src='/assets/sketch-1.svg' fill />
+          <div className="relative w-full">
+            <Image alt="Sketch images" src="/assets/sketch-1.svg" fill />
           </div>
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Paypen
+export default Paypen;
