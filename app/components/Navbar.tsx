1e731e77563d755daaa328a0ad4d79d700030a8d
Tobi Samuel <ufomatobi@gmail.com>
2023-07-09 22:37:44 +0100

    Fix animations on nav items

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import Menu from "./Menu";
+import { cn } from "@/lib/utils";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,9 +13,15 @@ const Navbar = () => {
   return (
     <nav className="text-dark left-0 top-0 z-[999] w-full">
       <div
-        className={`left-0 top-0 z-50 flex w-full justify-between ${
-          menuOpen ? "bg-text-dark text-text-gray" : "bg-white"
-        } px-4 py-8 transition ease-in-out lg:px-[100px]`}
+        // className={`left-0 top-0 z-50 flex w-full justify-between ${
+        //   menuOpen ? "bg-text-dark text-text-gray" : "bg-white"
+        // } px-4 py-8 transition ease-in-out lg:px-[100px]`}
+        className={cn(
+          "left-0 top-0 z-50 flex w-full justify-between bg-white px-4 py-8 transition ease-in-out lg:px-[100px]",
+          {
+            "bg-text-dark text-text-gray": menuOpen,
+          }
+        )}
       >
         <Link href="/" className="uppercase">
           <span>azeez</span>
@@ -23,25 +30,27 @@ const Navbar = () => {
         <div className="hidden gap-8 lg:flex">
           <Link
             href="/work"
-            className={`hover:underline-offset-6 uppercase hover:underline ${
-              pathname === "/work" ? "underline-offset-6 underline" : ""
-            }`}
+            className={cn(
+              "relative overflow-hidden pb-2 uppercase after:absolute after:bottom-1 after:left-0 after:h-0.5 after:w-full after:-translate-x-full after:bg-black after:transition after:duration-300 after:ease-in after:content-[''] after:hover:translate-x-0"
+            )}
           >
             work
           </Link>
 
           <Link
             href="/about"
-            className={`uppercase hover:underline hover:underline-offset-4 ${
-              pathname === "/about" ? "underline underline-offset-4" : ""
-            }`}
+            className={cn(
+              "relative overflow-hidden pb-2 uppercase after:absolute after:bottom-1 after:left-0 after:h-0.5 after:w-full after:-translate-x-full after:bg-black after:transition after:duration-300 after:ease-in after:content-[''] after:hover:translate-x-0"
+            )}
           >
             about
           </Link>
 
           <a
             href="https://drive.google.com/file/d/1w5wsr7QSCGXyIkh6-Lo4y4awrQ3vtGyA/view"
-            className="uppercase hover:underline hover:underline-offset-4"
+            className={cn(
+              "relative overflow-hidden pb-2 uppercase after:absolute after:bottom-1 after:left-0 after:h-0.5 after:w-full after:-translate-x-full after:bg-black after:transition after:duration-300 after:ease-in after:content-[''] after:hover:translate-x-0"
+            )}
             rel="noopener noreferrer"
             target="_blank"
           >
@@ -50,7 +59,9 @@ const Navbar = () => {
 
           <a
             href="mailto:isiakabdulazeez46@gmail.com"
-            className="uppercase hover:underline hover:underline-offset-4"
+            className={cn(
+              "relative overflow-hidden pb-2 uppercase after:absolute after:bottom-1 after:left-0 after:h-0.5 after:w-full after:-translate-x-full after:bg-black after:transition after:duration-300 after:ease-in after:content-[''] after:hover:translate-x-0"
+            )}
           >
             contact
           </a>
